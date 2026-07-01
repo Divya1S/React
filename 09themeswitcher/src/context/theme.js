3d24c38d89e0cc495639b7438c14ded02e481da3
Divya Rajput <divrajput736938@gmail.com>
2026-06-30 19:28:37 -0700

    theme switcher

---
+++ 09themeswitcher/src/context/theme.js
@@ -0,0 +1,13 @@
+import { createContext, useContext } from "react";
+
+export const ThemeContext = createContext({
+    themeMode: "light",
+    darkTheme: () => {},
+    lightTheme: () => {},
+});
+
+export const ThemeProvider = ThemeContext.Provider;
+
+export default function useTheme() {
+    return useContext(ThemeContext); //This allows to access the context value in any component that calls this hook
+}
